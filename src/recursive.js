834915971a2c902109899fc7ffed905fd66e71dc
Paul-Constantin Chitic <paul.constantin.chitic@gmail.com>
2018-05-17 19:53:26 +0300

    RecursiveJS:  update

--- src/recursive.js
+++ src/recursive.js
@@ -509,7 +509,7 @@ function tap(fn, x) {
 // TODO: this doesn't seem to be a classic transduce, will revisit it
 function transduce(ops, xs) {
   return (function transduce(ops, [x, ...xs], acc = []) {
-    const applyOps = ([op, ...ops], x) =>   op === undefined ? x : !op(x) ? undefined : applyOps(ops, op(x));
+    const applyOps = ([op, ...ops], x) => op === undefined ? x : !op(x) ? undefined : applyOps(ops, op(x));
 
     return x === undefined ? acc : transduce(ops, xs, [...acc, ...(applyOps(reverse(ops), [x]) || [])]);
   })(ops, xs);
