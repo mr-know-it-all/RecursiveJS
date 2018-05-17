96c41546b962f4586d4c15d7a68dc8fc53f08a04
Paul-Constantin Chitic <paul.constantin.chitic@gmail.com>
2018-05-17 19:51:38 +0300

    RecursiveJS: indentation update

--- src/recursive.js
+++ src/recursive.js
@@ -506,14 +506,13 @@ function tap(fn, x) {
 }
 
 // Functor F => transduce :: [F* -> F*, F* -> F*, ...] -> F* -> F*
-// TODO: this doesn't seem accurate, sill revisit it
+// TODO: this doesn't seem to be a classic transduce, will revisit it
 function transduce(ops, xs) {
-	return (function transduce(ops, [x, ...xs], acc = []) {
-		const applyOps = ([op, ...ops], x) =>
-			op === undefined ? x : !op(x) ? undefined : applyOps(ops, op(x));
+  return (function transduce(ops, [x, ...xs], acc = []) {
+    const applyOps = ([op, ...ops], x) =>   op === undefined ? x : !op(x) ? undefined : applyOps(ops, op(x));
 
-		return x === undefined ? acc : transduce(ops, xs, [...acc, ...(applyOps(reverse(ops), [x]) || [])]);
-	})(ops, xs);
+    return x === undefined ? acc : transduce(ops, xs, [...acc, ...(applyOps(reverse(ops), [x]) || [])]);
+  })(ops, xs);
 }
 
 // transpose :: [[a]] -> [[a]]
