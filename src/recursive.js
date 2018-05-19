45cf0121bd64208808edaa8cc2cb07b991e8985a
Paul-Constantin Chitic <paul.constantin.chitic@gmail.com>
2018-05-19 20:41:20 +0300

    RecursiveJS: _

--- src/recursive.js
+++ src/recursive.js
@@ -71,7 +71,7 @@ function applySpec(xo) {
   }
 }
 
-// String -> a -> {Key: v} -> {Key: v}
+// assoc :: String -> a -> {Key: v} -> {Key: v}
 function assoc(k, v, xo) {
   return (function assoc(k, v, [x, ...xo], acc = {}) {
     return x === undefined ? acc : assoc(k, v, xo, (acc[x[0]] = x[1], acc[k] = v, acc));
