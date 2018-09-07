c1c70e645298465c88bcaf2b2b9b1a9cd0028a39
Paul-Constantin Chitic <paul.constantin.chitic@gmail.com>
2018-09-07 11:47:48 +0300

    fix for insertionSort function

--- src/recursive.js
+++ src/recursive.js
@@ -594,7 +594,11 @@ function innerJoin(fn, xs, ys) {
 // insertionSort :: Ord a => [a] -> [a]
 function insertionSort(xs) {
   return (function insertionSort([x, ...xs], sorted = []) {
-    return x === undefined ? sorted : insertionSort(xs, quickSort([...sorted, x]));
+    return x === undefined ? sorted : insertionSort(xs, (function addToSorted([y, ...ys], acc = []) {
+      if(y === undefined) return [...acc, x];
+      if(x < y) return [...acc, x, y, ...ys];
+      return addToSorted(ys, [...acc, y]);
+    })(sorted));
   })(xs);
 }
 
