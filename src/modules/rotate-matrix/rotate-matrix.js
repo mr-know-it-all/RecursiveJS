829a6bb4b6d312447bd1ca9c8edf835263fc2942
Paul <paul.constantin.chitic@gmail.com>
2018-12-24 00:58:39 +0200

    RecursiveJS: updates for rotateMatrix

--- src/modules/rotate-matrix/rotate-matrix.js
+++ src/modules/rotate-matrix/rotate-matrix.js
@@ -24,20 +24,14 @@
 // function clockwise(matrix) { return matrix[0].map((_, i) => matrix.map(row => row[i]).reverse()); }
 // function counterClockwise(matrix) { return matrix[0].map((_, i) => matrix.map(row => row[i])).reverse(); }
 
-// // rotateMatrix :: ([[a]] -> String -> Int) -> [[a]]
-// function rotateMatrix(matrix = [], direction = 'clockwise', degrees = 90) {
-//   return (
-//     degrees > 90
-//       ? rotateMatrix(({clockwise, counterClockwise})[direction](matrix), direction, degrees - 90)
-//       : ({clockwise, counterClockwise})[direction](matrix)
-//   );
-// }
 
 const reverse = require('../reverse/reverse.js');
 const compose = require('../compose/compose.js');
 const transpose = require('../transpose/transpose.js');
 
+// clockwise :: [[a]] -> [[a]]
 function clockwise(matrix) { return compose(transpose, reverse)(matrix); }
+// counterClockwise :: [[a]] -> [[a]]
 function counterClockwise(matrix) { return compose(reverse, transpose)(matrix); }
 
 // rotateMatrix :: ([[a]] -> String -> Int) -> [[a]]
