523399245a01edd9ecf6926bf316c11d97924494
Paul <paul.constantin.chitic@gmail.com>
2019-06-02 16:53:50 +0300

    refactor transpose

--- src/modules/transpose/transpose.js
+++ src/modules/transpose/transpose.js
@@ -1,5 +1,6 @@
 const length = require('../length/length.js');
 const filter = require('../filter/filter.js');
+const forEach = require('../for-each/for-each.js');
 const map = require('../map/map.js');
 const reduce = require('../reduce/reduce.js');
 
@@ -8,10 +9,17 @@ function transpose(xs) {
   let maxIndex = reduce((acc, v) => length(v) > acc ? length(v) : acc, xs, 0) - 1;
 
   return (function transpose(index = 0, acc = []) {
-    // TODO: after forEach fix, use it instead of map
-    map(x => acc[index] = acc[index] ? [...acc[index], x[index] || null] : [x[index] || null], xs);
+    forEach(row => (
+      acc[index] = acc[index]
+        ? [...acc[index], row[index] || null]
+        : [row[index] || null]
+    ), xs);
 
-    return index === maxIndex ? map(xs => filter(x => x !== null, xs), acc) : transpose(index + 1, acc);
+    return (
+      index === maxIndex
+        ? map(xs => filter(Boolean, xs), acc)
+        : transpose(index + 1, acc)
+    );
   })();
 }
 
